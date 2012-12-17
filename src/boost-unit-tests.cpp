1847983be9497ba71c1a730e0f56fc1caf9dcb5f
Anders Damsgaard Christensen <adc@geo.au.dk>
2012-12-17 20:17:36 +0100

    Added empty boost unit test

---
+++ src/boost-unit-tests.cpp
@@ -0,0 +1,16 @@
+// Include the boost unit testing framework,
+// requires -lboost_unit_test_framework when linking
+#include <boost/test/included/unit_test.hpp>
+
+#include "sphere.h"
+
+// define the name of the test suite
+BOOST_AUTO_TEST_SUITE (spheretest)
+
+BOOST_AUTO_TEST_SUITE (inittest)
+{
+    DEM testDEM();
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
